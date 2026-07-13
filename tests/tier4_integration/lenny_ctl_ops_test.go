301beca11aa19c701db56df0f1be3daeaa014d4c
Joan Fargas <joan.aguilar.fargas@gmail.com>
2026-07-12 23:37:01 -0700

    test-gaps: gofumpt lenny_ctl_ops_test.go from the section-25 merge

--- tests/tier4_integration/lenny_ctl_ops_test.go
+++ tests/tier4_integration/lenny_ctl_ops_test.go
@@ -67,7 +67,8 @@ func TestLennyCtlOperabilityCommandsE2E(t *testing.T) {
 	})
 	rd := containers.StartRedis(t, containers.RedisOptions{})
 
-	ops := opsprocess.StartWith(t,
+	ops := opsprocess.StartWith(
+		t,
 		"--postgres-dsn="+pg.DSN,
 		"--redis-url=redis://"+rd.Addr+"/0",
 		"--redis-allow-insecure",
